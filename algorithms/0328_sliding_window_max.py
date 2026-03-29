f02ef56f6c9445320123acb0fdeed30ca167e1c8
EmmaSwan <emmaswan0729@gmail.com>
2026-03-29 21:43:49 +0100

    [algo] 0329 - teo sum

--- algorithms/0328_sliding_window_max.py
+++ algorithms/0328_sliding_window_max.py
@@ -21,4 +21,4 @@ def sliding_window_max(nums:list[int], k:int) -> list[int]:
     return results
 
 nums = [1, 3, -1, -3, 5, 3, 6, 7]
-print(sliding_window_max(nums, 3))
+print(sliding_window_max(nums, 3)) # [3, 3, 5, 5, 6, 7]
