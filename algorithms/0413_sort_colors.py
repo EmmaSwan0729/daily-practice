3d820832a0b482dc8388e848a82b56548635b0db
EmmaSwan <emmaswan0729@gmail.com>
2026-04-13 22:25:43 +0100

    Add Dutch National Flag solution for sort colors problem

---
+++ algorithms/0413_sort_colors.py
@@ -0,0 +1,19 @@
+nums = [2,0,2,1,1,0]
+
+def sort_colors(nums:list[int]) -> list[int]:
+    first, second, third = 0, 0, len(nums)-1
+    
+    while second <= third:
+        if nums[second] == 0:
+            nums[first], nums[second] = nums[second], nums[first]
+            first += 1
+            second += 1
+        elif nums[second] == 1:
+            second += 1
+        else:
+            nums[second], nums[third] = nums[third],nums[second]
+            third -= 1
+
+    return nums
+
+print(sort_colors(nums))
